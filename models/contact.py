0036965b75ac5c59a953b9c0530fa29fb2a9495b
pardodiazrodrigo <pardo.diaz.rodrigo@gmail.com>
2022-11-24 22:13:54 -0300

    MYSQL config

--- models/contact.py
+++ models/contact.py
@@ -2,6 +2,7 @@ from utils.db import db
 
 
 class Contact(db.Model):
+
     id = db.Column(db.Integer, primary_key=True)
     fullname = db.Column(db.String(100))
     email = db.Column(db.String(100))
--- routes/contacts.py
+++ routes/contacts.py
@@ -1,4 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
+from models.contact import Contact
+from utils.db import db
+
+
 
 contacts = Blueprint('contacts', __name__)
 
@@ -8,9 +12,18 @@ def home():
     return render_template('home.html')
 
 
-@contacts.route('/new')
+@contacts.route('/new', methods=['POST'])
 def add_contact():
-    return "Saving contact"
+    fullname = request.form['fullname']
+    email = request.form['email']
+    phone = request.form['phone']
+
+    new_contact = Contact(fullname=fullname, email=email, phone=phone)
+
+    db.session.add(new_contact)
+    db.session.commit()
+
+    return redirect('/')
 
 
 @contacts.route('/update')
